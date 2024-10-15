9a021ae9f789e26d7000337c4a125de42714bac6
Stevie Hryciw <codroid@gmail.com>
2024-10-15 14:44:55 -0700

    Linux fixes (#1)
    * Add Pulseaudio backend
    * Make Pulseaudio + ALSA the default backends on Linux
    * Update upstream to master, because the latest tag is from several years ago and does not include Pulseaudio
    * Fix system library names for ALSA and JACK

--- build.zig
+++ build.zig
@@ -8,13 +8,14 @@ pub const HostApi = enum {
     dsound,
     jack,
     oss,
+    pulseaudio,
     wasapi,
     wdmks,
     wmme,
 
     pub const defaults = struct {
         pub const macos: []const HostApi = &.{.coreaudio};
-        pub const linux: []const HostApi = &.{.alsa};
+        pub const linux: []const HostApi = &.{ .alsa, .pulseaudio };
         pub const windows: []const HostApi = &.{.wasapi};
     };
 };
@@ -92,7 +93,7 @@ pub fn build(b: *std.Build) !void {
                         try flags.append("-DPA_USE_ALSA=1");
                         lib.addIncludePath(pa.path("src/hostapi/alsa"));
                         lib.addCSourceFiles(.{ .root = pa_root, .files = src_hostapi_alsa });
-                        lib.linkSystemLibrary("alsa");
+                        lib.linkSystemLibrary("asound");
                     },
                     .asihpi => {
                         try flags.append("-DPA_USE_ASIHPI=1");
@@ -104,12 +105,18 @@ pub fn build(b: *std.Build) !void {
                         try flags.append("-DPA_USE_JACK=1");
                         lib.addIncludePath(pa.path("src/hostapi/jack"));
                         lib.addCSourceFiles(.{ .root = pa_root, .files = src_hostapi_jack });
-                        lib.linkSystemLibrary("jack2");
+                        lib.linkSystemLibrary("jack");
                     },
                     .oss => {
                         try flags.append("-DPA_USE_OSS=1");
                         lib.addIncludePath(pa.path("src/hostapi/oss"));
                         lib.addCSourceFiles(.{ .root = pa_root, .files = src_hostapi_oss });
+                    },
+                    .pulseaudio => {
+                        try flags.append("-DPA_USE_PULSEAUDIO=1");
+                        lib.addIncludePath(pa.path("src/hostapi/pulseaudio"));
+                        lib.addCSourceFiles(.{ .root = pa_root, .files = src_hostapi_pulseaudio });
+                        lib.linkSystemLibrary("pulse");
                     },
                     else => unsupportedHostApi(t.os.tag, api),
                 }
@@ -219,6 +226,12 @@ const src_hostapi_jack = &.{
 
 const src_hostapi_oss = &.{
     "src/hostapi/oss/pa_unix_oss.c",
+};
+
+const src_hostapi_pulseaudio = &.{
+    "src/hostapi/pulseaudio/pa_linux_pulseaudio.c",
+    "src/hostapi/pulseaudio/pa_linux_pulseaudio_block.c",
+    "src/hostapi/pulseaudio/pa_linux_pulseaudio_cb.c",
 };
 
 const src_hostapi_wasapi = &.{
